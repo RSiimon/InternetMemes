7512aeaa3726e25c8f7cb94ff5be1f5b375b0fda
Kristjan Lõhmus <lohmus@rushstreetinteractive.com>
2021-12-30 14:38:52 +0200

    Amend ingestion.py

--- cleaning2.py
+++ cleaning2.py
@@ -4,12 +4,11 @@
 
 import numpy as np
 import pandas as pd
-import json 
+import json
 from copy import deepcopy
 import string
 
-
-path_to_data = "data//"  
+path_to_data = "data//"
 path_to_cleaned = "cleaned_data//tmp_with_lists//"
 
 fname = "kym_vision.json"
@@ -18,13 +17,13 @@ fname_refs = 'refs_df.csv'
 fname_rel = 'relations_df.csv'
 fname_textual = 'textual_df.csv'
 
-
 # Loading data
 df = pd.read_json(path_to_data + fname, orient='index')
-df_main = pd.read_csv(path_to_cleaned + fname_main_df, sep=';', index_col=0, encoding='utf-8') 
-refs_df= pd.read_csv(path_to_cleaned + fname_refs, sep=';', index_col=0, encoding='utf-8')
-relations_df= pd.read_csv(path_to_cleaned + fname_rel, sep=';', index_col=0, encoding='utf-8')
-textual_df= pd.read_csv(path_to_cleaned + fname_textual, sep=';', index_col=0, encoding='utf-8')
+df_main = pd.read_csv(path_to_cleaned + fname_main_df, sep=';', index_col=0, encoding='utf-8')
+refs_df = pd.read_csv(path_to_cleaned + fname_refs, sep=';', index_col=0, encoding='utf-8')
+relations_df = pd.read_csv(path_to_cleaned + fname_rel, sep=';', index_col=0, encoding='utf-8')
+textual_df = pd.read_csv(path_to_cleaned + fname_textual, sep=';', index_col=0, encoding='utf-8')
+
 
 # Convert columns containing lists from str to list:
 def convert_list_col(col, df2):
@@ -32,78 +31,79 @@ def convert_list_col(col, df2):
     col2 = []
     for row in col_list:
         if isinstance(row, str):
-            col2.append(row.strip("[]'").split("', '")) 
+            col2.append(row.strip("[]'").split("', '"))
         else:
             col2.append(np.nan)
     df2[col] = col2
     return df2
 
-df_main = convert_list_col('similar_images', df_main) 
-df_main = convert_list_col('type', df_main) 
-df_main = convert_list_col('search_keywords', df_main) 
-relations_df = convert_list_col('siblings', relations_df) 
-relations_df = convert_list_col('children', relations_df) 
+
+df_main = convert_list_col('similar_images', df_main)
+df_main = convert_list_col('type', df_main)
+df_main = convert_list_col('search_keywords', df_main)
+relations_df = convert_list_col('siblings', relations_df)
+relations_df = convert_list_col('children', relations_df)
 # type(relations_df['siblings'].iloc[0])  # list
 
 # Convert 'year' column to int:
-df_main['year'] = df_main['year'].astype(pd.Int32Dtype()) 
-
+df_main['year'] = df_main['year'].astype(pd.Int32Dtype())
 
 # Original cleaned data title and url for joining 
-id_df = pd.DataFrame(data = df_main.index.tolist(), columns = ['url']) 
+id_df = pd.DataFrame(data=df_main.index.tolist(), columns=['url'])
 df.index = pd.Series([x.split('/')[-1] for x in df.index.tolist()])
 
-#Leave only rows present in main table:
+# Leave only rows present in main table:
 df = id_df.merge(df, left_on='url', right_index=True)
 df.index = df['url'].tolist()
 df = df.drop(columns=['url'])
 ids = df.index.tolist()
 
 # - There were 19 rows in main_df which were not in df:
-dropped = df_main.drop(ids).index.tolist() # drop later, with others
+dropped = df_main.drop(ids).index.tolist()  # drop later, with others
 
 # df.columns 
-#['labelAnnotations', 'safeSearchAnnotation', 'webDetection', 'error']
+# ['labelAnnotations', 'safeSearchAnnotation', 'webDetection', 'error']
 
 # -----------------------------------
 
 # 'ERROR' COLUMN:
-    
+
 # - Rows with bad image data:
-err_lab = df['error']  
-dropped+= err_lab.dropna().index.tolist()
-df = df.drop(columns = ['error'])
+err_lab = df['error']
+dropped += err_lab.dropna().index.tolist()
+df = df.drop(columns=['error'])
 
 # -----------------------------------
 
 # SAFE SEARCH ANNOTATIONS COLUMN:
 
 df_safe = df['safeSearchAnnotation'].apply(pd.Series)
-df_safe =df_safe.drop(columns=[0])
+df_safe = df_safe.drop(columns=[0])
 
 # Leave out some disturbing images:
-a = df_safe[df_safe['medical']=="VERY_LIKELY"] # leave all out (6)
+a = df_safe[df_safe['medical'] == "VERY_LIKELY"]  # leave all out (6)
 # a = df_safe[df_safe['medical']=="LIKELY"] # mostly ok
 # a = df_safe[df_safe['racy']=="VERY_LIKELY"] # mostly ok
 # a = df_safe[df_safe['spoof']=="VERY_LIKELY"] " ok
-b = df_safe[df_safe['violence']=="VERY_LIKELY"] # # leave all out (somehow disturbing) (13)
-c = df_safe[df_safe['adult']=="VERY_LIKELY"] # leave all out  (23)
+b = df_safe[df_safe['violence'] == "VERY_LIKELY"]  # # leave all out (somehow disturbing) (13)
+c = df_safe[df_safe['adult'] == "VERY_LIKELY"]  # leave all out  (23)
 
-dropped+=a.index.tolist()
-dropped+=b.index.tolist()
-dropped+=c.index.tolist()
-dropped = list(set(dropped)) # leave out dumplicates  # len 138
+dropped += a.index.tolist()
+dropped += b.index.tolist()
+dropped += c.index.tolist()
+dropped = list(set(dropped))  # leave out dumplicates  # len 138
 
 # -----------------------------------
 
 # DROP ROWS WITH BAD IMAGE DATA AND DISTURBING IMAGES FROM ALL TABLES, CONSOLIDATE TABLES SO THAT DF HAS SAME ROWS AS MAIN TABLE:
-    
-df = df[df.index.isin(dropped)==False]
-df_main = df_main[df_main.index.isin(dropped)==False]
-refs_df= refs_df[refs_df.index.isin(dropped)==False]
-relations_df = relations_df[relations_df.index.isin(dropped)==False]
-textual_df = textual_df[textual_df.index.isin(dropped)==False]
-df_safe = df_safe[df_safe.index.isin(dropped)==False]
+
+df = df[df.index.isin(dropped) == False]
+df_main = df_main[df_main.index.isin(dropped) == False]
+refs_df = refs_df[refs_df.index.isin(dropped) == False]
+relations_df = relations_df[relations_df.index.isin(dropped) == False]
+textual_df = textual_df[textual_df.index.isin(dropped) == False]
+df_safe = df_safe[df_safe.index.isin(dropped) == False]
+
 
 # In relations_df, also remove dropped ids from siblings/parents/children columns:
 def drop_values_parent(df2, dropped):
@@ -115,8 +115,10 @@ def drop_values_parent(df2, dropped):
         parent2.append(p)
     df2['parent'] = parent2
     return df2
-    
+
+
 relations_df = drop_values_parent(relations_df, dropped)
+
 
 def drop_values(df2, col, dropped):
     rows2 = []
@@ -126,15 +128,16 @@ def drop_values(df2, col, dropped):
             for el in row:
                 if el not in dropped:
                     row2.append(el)
-            if len(row2)==0:
-                row2 =np.nan
+            if len(row2) == 0:
+                row2 = np.nan
         else:
             row2 = np.nan
         rows2.append(row2)
     df2[col] = rows2
-    
+
     return df2
-        
+
+
 relations_df = drop_values(relations_df, 'siblings', dropped)
 relations_df = drop_values(relations_df, 'children', dropped)
 
@@ -180,7 +183,7 @@ vision_annot = []
 # nan_count = 0
 for row in annotations:
     row_labels, row_scores = [], []
-    if isinstance(row, list) and len(row)!=0: 
+    if isinstance(row, list) and len(row) != 0:
         for label in row:
             row_labels.append(label['description'])
             row_scores.append(label['score'])
@@ -188,12 +191,11 @@ for row in annotations:
     #     nan_count+=1  # 78 nan values
     vision_annot.append([row_labels, row_scores])
     vision_annot_labels.append(row_labels)
-    vision_annot_scores.append(row_scores)    
+    vision_annot_scores.append(row_scores)
 
 # Add to main table:
 df_main['image_recogn_labels'] = vision_annot_labels
 df_main['image_recogn_scores'] = vision_annot_scores
-
 
 # TODO: Save as json (add ids as keys)
 # -maybe: add "has_vison_annot" in main table
@@ -202,46 +204,45 @@ df_main['image_recogn_scores'] = vision_annot_scores
 # ---------------
 
 # Reverse annotations:
- # - to easily search for memes that have same annotation (label):
+# - to easily search for memes that have same annotation (label):
 
 # Gather all labels:
 all_labels = set()
 for row in vision_annot:
-    all_labels = all_labels.union(row[0]) 
-    
-all_labels = list(all_labels)  # len 4411
+    all_labels = all_labels.union(row[0])
 
+all_labels = list(all_labels)  # len 4411
 
 # Add memes to labels with conf. scores:
 
 vision_annot_rev = {}
 for label in all_labels:
     # vision_annot_rev[label] = {'memes': [], 'scores': []}
-    vision_annot_rev[label] = [] # to allow ordering
-  
+    vision_annot_rev[label] = []  # to allow ordering
+
 for i, row in enumerate(vision_annot):
-    if isinstance(row, list) and len(row[0])!=0: 
+    if isinstance(row, list) and len(row[0]) != 0:
         annots, scores = row[0], row[1]
         for j in range(len(annots)):
             vision_annot_rev[annots[j]].append([ids[i], scores[j]])
-        
+
 # values =  list(vision_annot_rev.values())
 # values2 = []
 for key, value in vision_annot_rev.items():
     # el = values[0]
-    d = pd.DataFrame(data = value)
-    d = d.sort_values(by=[1], ascending = False).T
+    d = pd.DataFrame(data=value)
+    d = d.sort_values(by=[1], ascending=False).T
     # values2.append([d.loc[0].tolist(), d.loc[1].tolist()])
     vision_annot_rev[key] = [d.loc[0].tolist(), d.loc[1].tolist()]
     # vision_annot_rev[key]['memes'] = d.loc[0].tolist()
     # vision_annot_rev[key]['scores'] =  d.loc[1].tolist()
 
 # Split into two dicts:
-a = {} #deepcopy(vision_annot_rev)
-b = {} #deepcopy(vision_annot_rev)
+a = {}  # deepcopy(vision_annot_rev)
+b = {}  # deepcopy(vision_annot_rev)
 for key, value in vision_annot_rev.items():
-    a[key]=value[0]
-    b[key]=value[1]
+    a[key] = value[0]
+    b[key] = value[1]
 
 a = pd.DataFrame(list(a.items()))
 b = pd.DataFrame(list(b.items()))
@@ -252,12 +253,11 @@ b = b.drop(columns=[0])
 vision_annot_rev_df = pd.concat([a, b], axis=1)
 vision_annot_rev_df.columns = ['memes', 'scores']
 
-
-# -----------------------------------  
+# -----------------------------------
 
 
 # WEB DETECTION COLUMN:
-    
+
 df_web = df['webDetection'].apply(pd.Series)
 
 df_web.columns
@@ -265,11 +265,11 @@ df_web.columns
 #        'partialMatchingImages', 'visuallySimilarImages', 'webEntities']
 
 
-#----------------
+# ----------------
 
 # - BEST GUESS LABELS
 
-wlab = df_web['bestGuessLabels'].tolist() 
+wlab = df_web['bestGuessLabels'].tolist()
 labels = []
 lang_codes = []
 
@@ -282,72 +282,67 @@ for row in wlab:
         lang_codes.append(row[0]['languageCode'])
     else:
         lang_codes.append(np.nan)
-        
+
 df_web['best_guess_label'] = labels
 df_web['best_guess_lang'] = lang_codes
 df_web = df_web.drop(columns=['bestGuessLabels'])
 
-
 # LEAVE OUT MEMES WITH NON-ENGLISH LANGUAGE CODE:
-    
+
 # - Often those memes were related to some public figures of specific countries. The language of the text on the meme was still in most cases English.
 # -> We decide to leave them out, since most people lack knowledge about country-specific context, and would not understand the meme without reading the description. 
 
 non_english = []
 ids = df.index.tolist()
 for i, el in enumerate(df_web['best_guess_lang'].tolist()):
-    if isinstance(el, str)==True and el!='en':
+    if isinstance(el, str) == True and el != 'en':
         non_english.append(ids[i])
 
 dropped = non_english
 
-df = df[df.index.isin(dropped)==False]
-df_main = df_main[df_main.index.isin(dropped)==False]
-refs_df= refs_df[refs_df.index.isin(dropped)==False]
-relations_df = relations_df[relations_df.index.isin(dropped)==False]
-textual_df = textual_df[textual_df.index.isin(dropped)==False]
-df_safe = df_safe[df_safe.index.isin(dropped)==False]
-vision_annot_rev_df = vision_annot_rev_df[vision_annot_rev_df.index.isin(dropped)==False]
+df = df[df.index.isin(dropped) == False]
+df_main = df_main[df_main.index.isin(dropped) == False]
+refs_df = refs_df[refs_df.index.isin(dropped) == False]
+relations_df = relations_df[relations_df.index.isin(dropped) == False]
+textual_df = textual_df[textual_df.index.isin(dropped) == False]
+df_safe = df_safe[df_safe.index.isin(dropped) == False]
+vision_annot_rev_df = vision_annot_rev_df[vision_annot_rev_df.index.isin(dropped) == False]
 
 relations_df = drop_values_parent(relations_df, dropped)
 relations_df = drop_values(relations_df, 'siblings', dropped)
 relations_df = drop_values(relations_df, 'children', dropped)
 
-df_web = df_web[df_web.index.isin(dropped)==False]
-df_web = df_web.drop(columns = ['best_guess_lang'])
+df_web = df_web[df_web.index.isin(dropped) == False]
+df_web = df_web.drop(columns=['best_guess_lang'])
 
-
-# From relations_df, Leave out any rows where there are now only nans: 
-a = np.sum(relations_df.isna(), axis =1)
-a = a[a==3].index.tolist()
-relations_df = relations_df.dropna(axis=0, how = 'all')
+# From relations_df, Leave out any rows where there are now only nans:
+a = np.sum(relations_df.isna(), axis=1)
+a = a[a == 3].index.tolist()
+relations_df = relations_df.dropna(axis=0, how='all')
 for el in a:
-	df_main['has_relations'].loc[a]=False
-
-
+    df_main['has_relations'].loc[a] = False
 
 # ADD BEST GUESS LABEL TO SEARCH KEYWORDS:
-df_main['best_guess_label'] =df_web['best_guess_label']
+df_main['best_guess_label'] = df_web['best_guess_label']
 # a = df_main[['type', 'search_keywords', 'title', 'best_guess_label']]
 search = df_main['search_keywords'].tolist()
 best_guess = df_main['best_guess_label'].tolist()
 
 for i, row in enumerate(best_guess):
-    if isinstance(row, str)==True:
+    if isinstance(row, str) == True:
         search_row = search[i]
-        if len(search_row)==1 and search_row[0]=="":
-            search[i]=[row]
+        if len(search_row) == 1 and search_row[0] == "":
+            search[i] = [row]
         else:
             search[i].append(row)
 
 df_main['search_keywords'] = search
-df_main = df_main.drop(columns = ['best_guess_label'])
+df_main = df_main.drop(columns=['best_guess_label'])
 
-
-#----------------
+# ----------------
 
 # - FULL MATCHING IMAGES and PARTIAL MATCHING IMAGES:
-    
+
 # 'Full matching images' are alternative urls of the same image all over the web (including KYM website, which we already have). KYM often has same image in different sizes (original, mobile, facebook). Those links might become important, because queries to KYM website seem to be limited and making too many queries per minute may result in blocking of IP (it already once happened for us). 
 
 # 'Partial matching images' seem also to be alternative urls to the same image with minor differences (sometimes with an additional text/caption, or slightly different text, or somewhat lower quality). Basically, they are still the same memes. 
@@ -360,8 +355,9 @@ df_main = df_main.drop(columns = ['best_guess_label'])
 
 # -> Add partially matching images to image urls in cases where we have less than six alternative image urls. Again, exclude urls to KYM (we already have those). 
 
-df_wmatch = df_web['fullMatchingImages'].tolist() 
+df_wmatch = df_web['fullMatchingImages'].tolist()
 df_wmatch3 = df_web['partialMatchingImages'].tolist()
+
 
 def collect_urls(df2, include_kym, max_urls):
     # df2 = wsim
@@ -369,22 +365,22 @@ def collect_urls(df2, include_kym, max_urls):
     for i, row in enumerate(df2):
         # i = 0
         # row = df2[0]
-        if isinstance(row, list)==True and len(row)!=0:
+        if isinstance(row, list) == True and len(row) != 0:
             counts = 0
             row_urls = []
-            for el in row: # dict
+            for el in row:  # dict
                 # el = row[0]
                 link = el['url']
-                if include_kym==False:
+                if include_kym == False:
                     if "kym-cdn" not in link:
                         row_urls.append(link)
-                        counts+=1
+                        counts += 1
                 else:
                     row_urls.append(link)
-                    counts+=1                        
-                if counts==max_urls:
+                    counts += 1
+                if counts == max_urls:
                     break
-            if len(row_urls)!=0:
+            if len(row_urls) != 0:
                 urls.append(row_urls)
             else:
                 urls.append(np.nan)
@@ -392,18 +388,19 @@ def collect_urls(df2, include_kym, max_urls):
             urls.append(np.nan)
     return urls
 
-urls =collect_urls(df_wmatch, False, 6)
-urls2 =collect_urls(df_wmatch3, False, 6)
+
+urls = collect_urls(df_wmatch, False, 6)
+urls2 = collect_urls(df_wmatch3, False, 6)
 urls3 = []
 
 for i, row in enumerate(urls):
-    if isinstance(row, list)==False:
+    if isinstance(row, list) == False:
         urls3.append(urls2[i])
-    elif len(row)>=6:
+    elif len(row) >= 6:
         urls3.append(row)
     else:
         # break
-        urls3.append(row + urls2[:6-len(row)])
+        urls3.append(row + urls2[:6 - len(row)])
 
 # ids = df.index.tolist()
 
@@ -411,8 +408,7 @@ for i, row in enumerate(urls):
 df_main['alt_img_urls'] = urls
 df_web = df_web.drop(columns=['fullMatchingImages', 'partialMatchingImages'])
 
-
-#----------------
+# ----------------
 
 # PAGES WITH MATCHING IMAGES:
 
@@ -420,13 +416,13 @@ df_web = df_web.drop(columns=['fullMatchingImages', 'partialMatchingImages'])
 # -> pages where matching images were found. NOT RELEVANT -> LEAVE OUT
 df_web = df_web.drop(columns=['pagesWithMatchingImages'])
 
-#----------------
+# ----------------
 
 # - VISUALLY SIMILAR IMAGES:  -> REALLY USEFUL!
-    
+
 # -> These are derivatives of the original meme; similar, but not the same.
 
-wsim = df_web['visuallySimilarImages'].tolist()  
+wsim = df_web['visuallySimilarImages'].tolist()
 # sim = df_main['similar_images'].tolist() # another set of similar images
 
 # Add max 10 similar images (only six had more than 10):
@@ -436,26 +432,26 @@ df_web = df_web.drop(columns=['visuallySimilarImages'])
 
 a = df_main.isna().sum()
 
-#----------------
+# ----------------
 
 # WEB ENTITIES:
-    
+
 # Here, we have keywords with scores. The keywords describe image category rather than what is on the image. 
 
 # -> leave out scores. 
 
-web_entities= df_web['webEntities'].tolist() 
+web_entities = df_web['webEntities'].tolist()
 types = df_main['type'].tolist()
 
 # Collect keyowrds:
 keywords = []
 for i, row in enumerate(web_entities):
-    if isinstance(row, list)==True and len(row)!=0:
+    if isinstance(row, list) == True and len(row) != 0:
         row_keywords = []
         for el in row:
             if 'description' in el.keys():
                 row_keywords.append(el['description'])
-        if len(row_keywords)!=0:
+        if len(row_keywords) != 0:
             keywords.append(row_keywords)
         else:
             keywords.append(np.nan)
@@ -464,21 +460,18 @@ for i, row in enumerate(web_entities):
 
 df_main['type2'] = keywords
 
-
-# -----------------------------------  
+# -----------------------------------
 
 # SAVE ALL:
 
-df_main.to_csv(path_to_cleaned + 'main_df.csv', sep=';', index=True, encoding='utf-8') 
-refs_df.to_csv(path_to_cleaned + 'refs_df.csv', sep=';', index=True, encoding='utf-8') 
-relations_df.to_csv(path_to_cleaned + 'relations_df.csv', sep=';', index=True, encoding='utf-8') 
-textual_df.to_csv(path_to_cleaned + 'textual_df.csv', sep=';', index=True, encoding='utf-8') 
-vision_annot_rev_df.to_csv(path_to_cleaned + 'vision_annot_df.csv', sep=';', index=True, encoding='utf-8') 
+df_main.to_csv(path_to_cleaned + 'main_df.csv', sep=';', index=True, encoding='utf-8')
+refs_df.to_csv(path_to_cleaned + 'refs_df.csv', sep=';', index=True, encoding='utf-8')
+relations_df.to_csv(path_to_cleaned + 'relations_df.csv', sep=';', index=True, encoding='utf-8')
+textual_df.to_csv(path_to_cleaned + 'textual_df.csv', sep=';', index=True, encoding='utf-8')
+vision_annot_rev_df.to_csv(path_to_cleaned + 'vision_annot_df.csv', sep=';', index=True, encoding='utf-8')
 
 # Load data:
 # df = pd.read_csv('main_df.csv', sep=';', index_col=0, encoding='utf-8')       
 
 
-
 # -----------------------------------
-
--- dags/ingestion.py
+++ dags/ingestion.py
@@ -2,6 +2,7 @@ import datetime
 
 import airflow
 import requests
+from airflow.operators.dummy import DummyOperator
 from faker import Faker
 import os
 from airflow import DAG
@@ -15,11 +16,15 @@ warnings.filterwarnings('ignore')
 
 fake = Faker()
 log_tag = "[INGESTION] "
-target = "kym.json"
-base_url = 'https://owncloud.ut.ee/owncloud/index.php/s/g4qB5DZrFEz2XLm/download/kym.json'
+target_1 = "kym.json"
+target_2 = "kym_spotlight.json"
+target_3 = "kym_vision.json"
+base_url = 'https://owncloud.ut.ee/owncloud/index.php/s/g4qB5DZrFEz2XLm/download/'
 client = MongoClient('mongodb://mongo:27017')  # Docker client
 db = client["memes"]
-collection = db["data"]
+collection_1 = db["kym"]
+collection_2 = db["kym_spotlight"]
+collection_3 = db["kym_vision"]
 
 default_args_dict = {
     'start_date': airflow.utils.dates.days_ago(0),
@@ -37,11 +42,11 @@ ingestion_dag = DAG(
 )
 
 
-def download_file():
+def download_file(target):
     """
     Checks if the file is present and depending on that marks as done or downloads it
     """
-    print(log_tag + "Started the download file function")
+    print(log_tag + "Started the download file function with target:", target)
 
     file_valid = True
     if os.path.isfile(target):
@@ -53,10 +58,12 @@ def download_file():
             print(log_tag + "File empty, overwriting")
     if not file_valid:
         print(log_tag + "Downloading the file")
-        r = requests.get(base_url, allow_redirects=True)
-        print(log_tag + "Download finished, writing into the file")
+        r = requests.get(base_url + target, allow_redirects=True)
+        print(log_tag + "Got response from URL:", r.status_code)
+        print(log_tag + "Check the data pulled", r.json()[0])
         with open(target, 'w+', encoding='utf-8') as f:
             json.dump(r.json(), f, ensure_ascii=False)
+            print(log_tag + "Dumped json into the file")
 
 
 def write_to_mongo():
@@ -64,25 +71,70 @@ def write_to_mongo():
     Inserts downloaded file into mongoDB
     """
 
-    with open(target, 'r+', encoding='utf-8') as f:
+    with open(target_1, 'r+', encoding='utf-8') as f:
         data = json.load(f)
-        collection.insert_many(data)
-        print(log_tag, "Test if the data is present:", db.data.find_one())
+        collection_1.insert_many(data)
+        print(log_tag, "Test if the data is present:", db.kym.find_one())
 
-task_one = PythonOperator(
-    task_id='download_file',
+    with open(target_2, 'r+', encoding='utf-8') as f:
+        data = json.load(f)
+        collection_2.insert_many(data)
+        print(log_tag, "Test if the data is present:", db.kym_spotlight.find_one())
+
+    with open(target_3, 'r+', encoding='utf-8') as f:
+        data = json.load(f)
+        collection_2.insert_many(data)
+        print(log_tag, "Test if the data is present:", db.kym_vision.find_one())
+
+
+read_first = PythonOperator(
+    task_id='download_file_1',
     dag=ingestion_dag,
     python_callable=download_file,
+    op_kwargs={
+        "target": target_1
+    },
+    trigger_rule='all_success',
+    depends_on_past=False,
+)
+read_second = PythonOperator(
+    task_id='download_file_2',
+    dag=ingestion_dag,
+    python_callable=download_file,
+    op_kwargs={
+        "target": target_2
+    },
     trigger_rule='all_success',
     depends_on_past=False,
 )
 
-task_two = PythonOperator(
+read_third = PythonOperator(
+    task_id='download_file_3',
+    dag=ingestion_dag,
+    python_callable=download_file,
+    op_kwargs={
+        "target": target_3
+    },
+    trigger_rule='all_success',
+    depends_on_past=False,
+)
+
+write_mongo = PythonOperator(
     task_id='write_into_mongo',
     dag=ingestion_dag,
     python_callable=write_to_mongo,
     trigger_rule='all_success',
     depends_on_past=False,
 )
+START = DummyOperator(
+    task_id='start_pipeline',
+    dag=ingestion_dag
+)
+COMPLETE = DummyOperator(
+    task_id='end_pipeline',
+    dag=ingestion_dag
+)
 
-task_one >> task_two
+START >> [read_first, read_second, read_third]
+[read_first, read_second, read_third] >> write_mongo
+write_mongo >> COMPLETE
